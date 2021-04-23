251109d1e8632292fdba543e451cd80a7f5cc73d
Victor <kd19421@bristol.ac.uk>
2021-04-22 22:31:51 -1200

    split and randomise data points - cross-validation

--- datafiles/lsr.py
+++ datafiles/lsr.py
@@ -2,11 +2,12 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import random
 from matplotlib import pyplot as plt
 from utilities import load_points_from_file as lpf
 from utilities import view_data_segments as vds
 
-global number_of_segments_to_be_ploted, ax, xs, ys
+global number_of_segments_to_be_ploted, ax, xs, ys, cross_validation_x, cross_validation_y
 
 """
 def create_path(selectedFile): 
@@ -27,7 +28,7 @@ def create_path(selectedFile):
     universalPath += selectedFile
     return universalPath
     
-xs, ys = lpf(create_path("basic_1.csv"))  
+xs, ys = lpf(create_path("noise_3.csv"))  
 
 fig, ax = plt.subplots()
 ax.set_xlabel("$x_\lambda$")
@@ -76,3 +77,52 @@ for i in range(number_of_segments_to_be_ploted):
     ax.legend()
 
 vds(xs, ys)     
+
+def square_error(true_y, estimated_y):
+    return np.sum((y - y_hat) ** 2)
+
+def shuffle_chunks(array):
+        random.shuffle(array)
+        return array    
+
+def cross_validation():
+    chunk_data = np.linspace(0, 20, 20)
+    shuffle_chunks(chunk_data)
+    chunk_data_random_x = []
+    chunk_data_random_y = []
+    
+    for i in range(20):
+        chunk_data_random_x = np.append(chunk_data_random_x, chunk_data[i])
+        chunk_data_random_y = np.append(chunk_data_random_y, chunk_data[i])
+
+    train_x_data = np.zeros((15,), dtype=int)
+    test_x_data = np.zeros((5,), dtype=int)
+    train_y_data = np.zeros((15,), dtype=int)
+    test_y_data = np.zeros((5,), dtype=int)
+
+    cross_validation_x = chunk_data_random_x[:5]
+    cross_validation_x = np.vstack(cross_validation_x, chunk_data_random_x[5:10])
+    cross_validation_x = np.vstack(cross_validation_x, chunk_data_random_x[10:15])
+    cross_validation_x = np.vstack(cross_validation_x, chunk_data_random_x[15:])
+
+    cross_validation_y = chunk_data_random_y[:5]
+    cross_validation_y = np.vstack(cross_validation_y, chunk_data_random_y[5:10])
+    cross_validation_y = np.vstack(cross_validation_y, chunk_data_random_y[10:15])
+    cross_validation_y = np.vstack(cross_validation_y, chunk_data_random_y[15:])
+
+    for i in range(4):
+        new_train_x_data = []
+        new_test_x_data = []
+        new_train_y_data = []
+        new_test_y_data = []
+        new_test_x_data = cross_validation_x[i]
+        new_test_y_data = cross_validation_y[i]
+        for j in range(4):
+            if (j != i):
+                new_train_x_data = np.append(new_train_x_data, cross_validation_x[j])
+                new_train_y_data = np.append(new_train_y_data, cross_validation_y[j])
+        train_x_data = np.vstack([train_x_data, new_train_x_data])  
+        train_y_data = np.vstack([train_y_data, new_train_y_data])      
+        test_x_data = np.vstack([test_x_data, new_test_x_data])  
+        test_y_data = np.vstack([test_y_data, new_test_y_data]) 
+    return train_x_data, train_y_data, test_x_data, test_y_data       
